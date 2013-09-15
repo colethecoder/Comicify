7f413044fd9071cb53a438321cc1de481467614e
Tom Cole <colethecoder@gmail.com>
2013-09-15 22:58:32 +0000

    reworked to cache 5 pages at a time

--- Comicify/Web/comicify.js
+++ Comicify/Web/comicify.js
@@ -36,9 +36,7 @@
         self.CurrentPageNumber(1);
         self.ComicSelected(true);
         self.ModeSwitch(false);
-        $('#comicPage').load(function() {
-            ResizeImage();
-        });
+        self.LoadFirstPage();
     };
 
     self.deselectComic = function() {
@@ -50,6 +48,78 @@
         self.LoadFolderContent();
     });
 
+    self.CurrentPageNumber.subscribe(function (newValue) {
+        self.LoadFirstPage();
+    });
+
+    self.LoadFirstPage = function () {
+        var pagesToLoad = [];
+        if (self.CurrentPageNumber() < 3) {
+            pagesToLoad = [1, 2, 3, 4, 5];
+        } else {
+            pagesToLoad = [self.CurrentPageNumber() - 2, self.CurrentPageNumber() - 1, self.CurrentPageNumber(), self.CurrentPageNumber() + 1, self.CurrentPageNumber() + 2];
+        }
+        var loadingArray = [];
+        var toLoadArray = [];
+        for (var i = 0; i < pagesToLoad.length; i++) {
+            var pagePath = self.CurrentPagePath(pagesToLoad[i]);
+            var found = false;
+            for (var j = 0; j < self.PageList.length; j++) {
+                if ($('img#'+self.PageList[j]).attr('src') == pagePath) {
+                    loadingArray.push({ number: pagesToLoad[i], page: self.PageList[j], load: false});
+                    found = true;
+                }
+            }
+            if (!found) {
+                toLoadArray.push(pagesToLoad[i]);
+            }
+        }
+
+        var toLoadPagesArray = [];
+
+        for (var j = 0; j < self.PageList.length; j++) {
+            $('img#' + self.PageList[j]).hide();
+            var foundPage = false;
+            for (var i = 0; i < loadingArray.length; i++) {
+                if (self.PageList[j] == loadingArray[i].page) {
+                    foundPage = true;
+                }
+            }
+            if (!foundPage) {
+                toLoadPagesArray.push(self.PageList[j]);
+            }
+        }
+
+        for (var i = 0; i < toLoadArray.length; i++) {
+            loadingArray.push({number: toLoadArray[i], page: toLoadPagesArray[i], load: true});
+        }
+        console.log(loadingArray);
+        for (var i = 0; i < loadingArray.length; i++) {
+            if (loadingArray[i].load) {
+                if (loadingArray[i].number == self.CurrentPageNumber()) {
+                    $('img#' + loadingArray[i].page).one('load', function() {
+                        console.log($(this));
+                        $(this).show();
+                        ResizeImage($(this));
+                    }
+                    ).attr('src', self.CurrentPagePath(loadingArray[i].number));
+                } else {
+                    $('img#' + loadingArray[i].page).one('load', function () {
+                        console.log($(this));
+                    }
+                    ).attr('src', self.CurrentPagePath(loadingArray[i].number));
+                }
+            }
+            if (loadingArray[i].number == self.CurrentPageNumber()) {
+                $('img#' + loadingArray[i].page).show();
+                ResizeImage($('img#' + loadingArray[i].page));
+                console.log(loadingArray[i].page);
+            }
+        }
+    };
+
+    self.PageList = ['comicPageA', 'comicPageB', 'comicPageC', 'comicPageD', 'comicPageE'];
+
     self.LoadFolderContent = function () {
         if (!self.ModeSwitch()) {
             $.getJSON('../api/Folder/' + self.Path(), null, function (data) {
@@ -59,25 +129,9 @@
         }
     };
 
-    self.CurrentPagePath = ko.computed(function() {
-        return '../api/Comic/' + self.Path() + '?page=' + self.CurrentPageNumber();
-    }, this);
-    
-    self.CurrentPagePathMinus1 = ko.computed(function () {
-        return '../api/Comic/' + self.Path() + '?page=' + (self.CurrentPageNumber() == 1 ? 1 : self.CurrentPageNumber() -1);
-    }, this);
-    
-    self.CurrentPagePathMinus2 = ko.computed(function () {
-        return '../api/Comic/' + self.Path() + '?page=' + (self.CurrentPageNumber() > 2 ? self.CurrentPageNumber() - 2 : 1);
-    }, this);
-
-    self.CurrentPagePathPlus1 = ko.computed(function () {
-        return '../api/Comic/' + self.Path() + '?page=' + (self.CurrentPageNumber() + 1);
-    }, this);
-    
-    self.CurrentPagePathPlus2 = ko.computed(function () {
-        return '../api/Comic/' + self.Path() + '?page=' + (self.CurrentPageNumber() + 2);
-    }, this);
+    self.CurrentPagePath = function(pageNumber) {
+        return '../api/Comic/' + self.Path() + '?page=' + pageNumber;
+    };
     
     self.PreviousPath = ko.computed(function () {
         var pathArray = self.Path().split("/");
@@ -101,15 +155,51 @@ var viewModel = new ViewModel();
 $(function () {
     ko.applyBindings(viewModel);
     
-    $('#comic').on("swipeleft","#comicPage", function (event) {
+    $('#comic').on("swipeleft","#comicPageA", function (event) {
+        viewModel.NextPage();
+    });
+    $('#comic').on("swipeleft", "#comicPageB", function (event) {
+        viewModel.NextPage();
+    });
+    $('#comic').on("swipeleft", "#comicPageC", function (event) {
+        viewModel.NextPage();
+    });
+    $('#comic').on("swipeleft", "#comicPageD", function (event) {
+        viewModel.NextPage();
+    });
+    $('#comic').on("swipeleft", "#comicPageE", function (event) {
         viewModel.NextPage();
     });
     
-    $('#comic').on("swiperight", "#comicPage", function (event) {
+    $('#comic').on("swiperight", "#comicPageA", function (event) {
+        viewModel.PreviousPage();
+    });
+    $('#comic').on("swiperight", "#comicPageB", function (event) {
+        viewModel.PreviousPage();
+    });
+    $('#comic').on("swiperight", "#comicPageC", function (event) {
+        viewModel.PreviousPage();
+    });
+    $('#comic').on("swiperight", "#comicPageD", function (event) {
+        viewModel.PreviousPage();
+    });
+    $('#comic').on("swiperight", "#comicPageE", function (event) {
         viewModel.PreviousPage();
     });
 
-    $('#comic').on("tap", "#comicPage", function(event) {
+    $('#comic').on("tap", "#comicPageA", function (event) {
+        viewModel.deselectComic();
+    });
+    $('#comic').on("tap", "#comicPageB", function (event) {
+        viewModel.deselectComic();
+    });
+    $('#comic').on("tap", "#comicPageC", function (event) {
+        viewModel.deselectComic();
+    });
+    $('#comic').on("tap", "#comicPageD", function (event) {
+        viewModel.deselectComic();
+    });
+    $('#comic').on("tap", "#comicPageE", function (event) {
         viewModel.deselectComic();
     });
     
@@ -124,19 +214,19 @@ $(function () {
         }
     });
     
-    
-
-    ResizeImage();
-    
     $(window).resize(function () {
-        ResizeImage();
+        ResizeImage($('img#comicPageA'));
+        ResizeImage($('img#comicPageB'));
+        ResizeImage($('img#comicPageC'));
+        ResizeImage($('img#comicPageD'));
+        ResizeImage($('img#comicPageE'));
     });
     
 
 });
 
-function ResizeImage() {
-    var $i = $('img#comicPage');
+function ResizeImage($i) {
+    console.log('Resize '+$i.attr('id'));
     var $c = $i.parent();
     $c.height($(window).height()-10);
     var i_ar = $i.width() / $i.height(), c_ar = $c.width() / $c.height();    
